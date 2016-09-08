b74f9f1f3b9ce2c637be801eab2d4e44f5e5ddf7
MedAdApp <med.adherence.app@gmail.com>
2016-09-08 13:03:37 -0400

    Yeshy's updates added and Survey updates
    Add time logs and survey updates

---
+++ app/src/main/java/com/nonscirenefas/yeshy/surveyapp1/MedicationInformationActivity.java
@@ -0,0 +1,141 @@
+package com.nonscirenefas.yeshy.surveyapp1;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.ListView;
+
+/**
+ * Created by Yeshy on 7/12/2016.
+ */
+public class MedicationInformationActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+    Context ctx;
+    public static final String PREFS_NAME = "MyPrefsFile";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_medication_information);
+        ctx = this;
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setSubtitle("Medication Information");
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        initializeMedList();
+    }
+
+    private void initializeMedList(){
+        String [] mArray = getResources().getStringArray(R.array.medicationArray);
+        System.out.println(mArray.toString());
+        final ListView lv = (ListView) findViewById(R.id.medicationListView);
+        lv.setAdapter(new MedicationAdapter(MedicationInformationActivity.this, mArray));
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    /*
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    */
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        if (id  == R.id.nav_home){
+            Intent i = new Intent(this, MainActivity.class);
+            startActivity(i);
+        }
+        else if (id == R.id.nav_bloodpressure) {
+            Intent i = new Intent(this, BloodPressureActivity.class);
+            startActivity(i);
+        }  else if (id == R.id.nav_medication) {
+            Intent i = new Intent(this, MedicationActivity.class);
+            startActivity(i);
+
+        }else if (id == R.id.nav_surveys) {
+            Intent i = new Intent(this, SurveySelectionActivity.class);
+            startActivity(i);
+
+        } else if (id == R.id.nav_callmypharmacist) {
+            Intent i = new Intent(Intent.ACTION_DIAL);
+            i.setData(Uri.parse("tel:6783600636"));
+            startActivity(i);
+        } else if (id == R.id.nav_logout) {
+            SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+            String UIDstored = settings.getString("UID", "Default");
+            Log.e("logout", UIDstored);
+
+            SharedPreferences.Editor editor = settings.edit();
+            editor.putString("UID", "Default");
+            editor.commit();
+
+            UIDstored = settings.getString("UID", "Default");
+            Log.e("logout", UIDstored);
+            Intent i = new Intent(this, LoginActivity.class);
+            startActivity(i);
+            finish();
+        }
+        else if (id == R.id.nav_hipaa) {
+            Intent i = new Intent(this, HIPAAActivity.class);
+            startActivity(i);
+
+        }
+        else if (id == R.id.nav_informedconsent) {
+            Intent i = new Intent(this, InformedConsentActivity.class);
+            startActivity(i);
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
